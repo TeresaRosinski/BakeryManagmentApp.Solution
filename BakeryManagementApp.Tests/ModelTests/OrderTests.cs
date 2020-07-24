d584bc94f09612996b4ab0aabac99ab35a05cf0f
Teresa Rosinski <TeresaRosinski@users.noreply.github.com>
2020-07-24 15:18:44 -0500

    add failing contructor price/title/description test

--- BakeryManagementApp.Tests/ModelTests/OrderTests.cs
+++ BakeryManagementApp.Tests/ModelTests/OrderTests.cs
@@ -15,10 +15,24 @@ namespace BakeryManagementApp.Tests
     // }
 
     [TestMethod]
-    public void ItemConstructor_CreatesInstanceOfItem_Item()
+    public void OrderConstructor_CreatesInstanceOfOrder_Order()
     {
-      Order newOrder = new Order();
+      Order newOrder = new Order("Cake Off", "2 cakes", "$450");
       Assert.AreEqual(typeof(Order), newOrder.GetType());
+    }
+    [TestMethod]
+    public void GetTitleDescriptionPrice_ReturnsTitleDescriptionPrice_String()
+    {
+      string title = "Mary's Cakes";
+      string description = "5 yellow cakes";
+      string price = "$444";
+      Order newOrder = new Order(title, description, price);
+      string resultTitle = newOrder.Title; 
+      string resultDescription = newOrder.Description;
+      string resultPrice = newOrder.Price; 
+      Assert.AreEqual(title, resultTitle);
+      Assert.AreEqual(description, resultDescription);
+      Assert.AreEqual(price, resultPrice);
     }
 
     
