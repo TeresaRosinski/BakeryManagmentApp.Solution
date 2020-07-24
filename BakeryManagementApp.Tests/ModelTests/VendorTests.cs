54ce21864868e61bb97b78188999aed834895af0
Teresa Rosinski <TeresaRosinski@users.noreply.github.com>
2020-07-24 17:23:24 -0500

    add failing GetNameAndAbout_ReturnsNameAndAbout_String()

--- BakeryManagementApp.Tests/ModelTests/VendorTests.cs
+++ BakeryManagementApp.Tests/ModelTests/VendorTests.cs
@@ -17,8 +17,21 @@ namespace BakeryManagementApp.Tests
     [TestMethod]
     public void VendorConstructor_CreatesInstanceOfVendor_Vendor()
     {
-      Vendor newVendor = new Vendor();
+      Vendor newVendor = new Vendor("name", "about");
       Assert.AreEqual(typeof(Vendor), newVendor.GetType());
+    }
+
+    [TestMethod]
+    public void GetNameAndAbout_ReturnsNameAndAbout_String()
+    {
+      string name = "Ben's Bakery";
+      string about = "Chicago, Il.";
+      Vendor newVendor = new Vendor(name, about);
+      string resultName = newVendor.Name; 
+      string resultAbout = newVendor.About; 
+      Assert.AreEqual(name, resultName );
+      Assert.AreEqual(about, resultAbout);
+
     }
 
     
