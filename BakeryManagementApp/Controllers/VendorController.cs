efff8054d3120f01b2aec564b2ed10964dae6c64
Teresa Rosinski <TeresaRosinski@users.noreply.github.com>
2020-07-24 18:28:52 -0500

    add vendorController.cs

--- BakeryManagementApp/Controllers/VendorController.cs
+++ BakeryManagementApp/Controllers/VendorController.cs
@@ -5,5 +5,49 @@ using BakeryManagementApp.Models;
 
 namespace BakeryManagementApp.Controllers
 {
-  
+  public class VendorsController : Controller 
+  {
+    [HttpGet("/vendors")]
+
+    public ActionResult Index ()
+    {
+      List <Vendor> allVendors = Vendor.GetAll();
+      return View(allVendors);
+    }
+
+    [HttpGet("/vendors/new")]
+    public ActionResult New()
+    {
+      return View();
+    }
+    [HttpPost("/vendors")]
+    public ActionResult Create (string vendorName, string vendorAbout)
+    {
+      Vendor newVendor = new Vendor (vendorName, vendorAbout);
+      return RedirectToAction("Index");
+    }
+
+    [HttpGet("/vendors/{id}")]
+    public ActionResult Show (int id)
+    {
+      Dictionary <string, object> model = new Dictionary<string, object> ();
+      Vendor selectedVendor = Vendor.Find(id);
+      List<Order> vendorOrders = selectedVendor.Orders;
+      model.Add("vendor", selectedVendor);
+      model.Add("orders", vendorOrders); 
+      return View(model);
+    }
+    [HttpPost("/vendors/{vendorId}/orders")]
+    public ActionResult Create(int vendorId, string orderTitle, string orderPrice, string orderDescription)
+    {
+      Dictionary<string, object> model = new Dictionary<string, object>();
+      Vendor foundVendor = Vendor.Find(vendorId);
+      Order newOrder = new Order(orderTitle, orderDescription, orderPrice);
+      foundVendor.AddOrder(newOrder);
+      List<Order> vendorOrders = foundVendor.Orders;
+      model.Add("orders", vendorOrders);
+      model.Add("vednor", foundVendor);
+      return View("Show", model);
+    }
+  }
 }
