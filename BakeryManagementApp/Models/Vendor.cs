8d715baf44b508134a32aea475b982197e532861
Teresa Rosinski <TeresaRosinski@users.noreply.github.com>
2020-07-24 17:26:20 -0500

    add compelted Order.cs file

--- BakeryManagementApp/Models/Vendor.cs
+++ BakeryManagementApp/Models/Vendor.cs
@@ -4,6 +4,19 @@ namespace BakeryManagementApp.Models
 {
   public class Vendor
   {
+    private static List<Vendor> _instances = new List <Vendor> { };
+    public string Name; 
+    public string About; 
+
+    public int Id {get; }
     
+    public List<Order> Order {get; set; }
+
+    public Vendor (string vendorName, string vendorAbout)
+    {
+      Name = vendorName; 
+      About = vendorAbout;
+      
+    }
   }
 }
