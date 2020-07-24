d5db8e9b9eca92140c0e095881e4866e80b30037
Teresa Rosinski <TeresaRosinski@users.noreply.github.com>
2020-07-24 14:58:13 -0500

    add intial controllers, models and homepage

---
+++ BakeryManagementApp/Controllers/VendorController.cs
@@ -0,0 +1,9 @@
+using System.Collections.Generic;
+using System;
+using Microsoft.AspNetCore.Mvc;
+using BakeryManagementApp.Models;
+
+namespace BakeryManagementApp.Controllers
+{
+  
+}
---
+++ BakeryManagementApp/Models/Vendor.cs
@@ -0,0 +1,9 @@
+using System.Collections.Generic;
+
+namespace BakeryManagementApp.Models
+{
+  public class Vendor
+  {
+    
+  }
+}
